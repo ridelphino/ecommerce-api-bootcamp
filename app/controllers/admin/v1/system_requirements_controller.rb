481d8f2060bcd4a80eaf1e13acdef725c5c3c6c7
Ricardo Delphino <radelphino@gmail.com>
2022-10-07 16:31:22 -0300

    Continue System Requirements Controller (update, delete)

--- app/controllers/admin/v1/system_requirements_controller.rb
+++ app/controllers/admin/v1/system_requirements_controller.rb
@@ -1,5 +1,7 @@
 module Admin::V1
     class SystemRequirementsController < ApiController
+        before_action :load_system_requirement, only: [:update, :destroy]
+
         def index
             @system_requirements = SystemRequirement.all
         end
@@ -10,18 +12,33 @@ module Admin::V1
             save_system_requirement!
         end
 
+        def update
+            @system_requirement.attributes = system_requirement_params
+            save_system_requirement!
+        end
+
+        def destroy
+            @system_requirement.destroy!
+        rescue
+            render_error(fields: @system_requirement.errors.messages)
+        end
+
         private
 
-            def system_requirement_params
-                return {} unless params.has_key?(:system_requirement)
-                params.require(:system_requirement).permit(:name, :operational_system, :storage, :processor, :memory, :video_board)
-            end
+        def load_system_requirement
+            @system_requirement = SystemRequirement.find(params[:id])
+        end
 
-            def save_system_requirement!
-                @system_requirement.save!
-                render :show
-            rescue
-                render_error(fields: @system_requirement.errors.messages)
-            end
+        def system_requirement_params
+            return {} unless params.has_key?(:system_requirement)
+            params.require(:system_requirement).permit(:name, :operational_system, :storage, :processor, :memory, :video_board)
+        end
+
+        def save_system_requirement!
+            @system_requirement.save!
+            render :show
+        rescue
+            render_error(fields: @system_requirement.errors.messages)
+        end
     end
 end
